8a2a95af398dd6070ab84967d177644dbb28e9f2
KaelMlinek <km1694@princeton.edu>
2024-10-22 15:49:08 -0400

    cleaning up

--- Parsing PDF/ParsePDF.py
+++ Parsing PDF/ParsePDF.py
@@ -191,31 +191,6 @@ def store_intoArray(starts_College, filename):
 
 def Main():
     # Paths to the PDF files
-
-    """   if detect_os() == "Windows":
-        pdf_22 = "Parsing PDF\\PDF\\OrigAvailableRoomsList2022.pdf"
-        pdf_24 = "Parsing PDF\\PDF\\AvailableRoomsList2024.pdf"
-        pdf_23 = "Parsing PDF\\PDF\\AvailableRoomsList2023.pdf"
-
-        # Paths to the output text files
-        output22 = "Parsing PDF\\PDF\\22.txt"
-        output23 = "Parsing PDF\\PDF\\23.txt"  
-        output24 = "Parsing PDF\\PDF\\24.txt" 
-
-        output = "Parsing PDF\\PDF\\24rooms.txt" 
-
-    if detect_os() == "Mac":
-        pdf_22 = "Parsing PDF/PDF/OrigAvailableRoomsList2022.pdf"
-        pdf_24 = "Parsing PDF/PDF/AvailableRoomsList2024.pdf"
-        pdf_23 = "Parsing PDF/PDF/AvailableRoomsList2023.pdf"
-
-        # Paths to the output text files
-        output22 = "Parsing PDF/PDF/22.txt"
-        output23 = "Parsing PDF/PDF/23.txt"  
-        output24 = "Parsing PDF/PDF/24.txt" 
-
-        output = "Parsing PDF/PDF/24rooms.txt"  """
-   
     pdf_22 = Path("PDF/OrigAvailableRoomsList2022.pdf")
     pdf_24 = Path("PDF/AvailableRoomsList2024.pdf")
     pdf_23 = Path("PDF/AvailableRoomsList2023.pdf")
