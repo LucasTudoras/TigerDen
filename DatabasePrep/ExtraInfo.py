d5818d0af9451fba1908d2a0e43110f67dbb1844
cdmck <cmckenna003@gmail.com>
2024-10-24 16:15:49 -0400

    renamed

--- DatabasePrep/ExtraInfo.py
+++ DatabasePrep/ExtraInfo.py
@@ -3,3 +3,14 @@
 # distanceTo, AC, elevator, and
 # bathroom fields for the database
 
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EXTRA_INFO_PATH = (insertfilepath.envhere)
+
+extra_info_db = pd.read_excel(EXTRA_INFO_PATH)
+
+# Loop through each room to get its extra 
+# info and put this into a dictionary 
