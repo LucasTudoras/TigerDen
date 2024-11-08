6a694aaa7e7953f983e28607fdd5eeb434fcc5e6
Christian Altawil <ca5701@princeton.edu>
2024-11-07 21:23:10 -0500

    favorite is functional now

---
+++ Database/addfavorites.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def create_tables():
+    conn = sqlite3.connect('rooms.db')
+    cursor = conn.cursor()
+
+    # Create the favorites table if it doesn't already exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS favorites (
+            user_id INTEGER,
+            room_id INTEGER,
+            PRIMARY KEY (user_id, room_id),
+            FOREIGN KEY (room_id) REFERENCES rooms(id)
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    create_tables()
