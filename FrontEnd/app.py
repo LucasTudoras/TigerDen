9f6b3ffe32c6cb5ed030c3b9b946b54aec185211
KaelMlinek <km1694@princeton.edu>
2024-11-12 15:13:47 -0500

    small change app.py

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -3,7 +3,7 @@ import os
 
 
 def main():
-    port = int(os.environment.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     td.app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == '__main__':
