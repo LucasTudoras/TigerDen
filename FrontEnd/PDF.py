89da56f3e85366dcfc6bba37d945cfff70dbca4d
KaelMlinek <km1694@princeton.edu>
2024-11-14 14:34:20 -0500

    manual rollback

--- FrontEnd/PDF.py
+++ FrontEnd/PDF.py
@@ -22,8 +22,7 @@ def filter_text(text):
     return filtered_lines
 
 #extracting the actual text from the pdf
-def extract_text_from_pdf(pdf_path, college_names):
-    ret = ""
+def extract_text_from_pdf(pdf_path, output_file_path, college_names):
     x = 0
     # Open the PDF file
     with pdfplumber.open(pdf_path) as pdf:
@@ -49,12 +48,12 @@ def extract_text_from_pdf(pdf_path, college_names):
                     if any(college in line for college in college_names) :
                         # Write the previous line if it exists
                         if previous_line and x == 0:
-                            ret += previous_line + '\n'  # Write the previous line
+                            write_text_to_file(output_file_path, previous_line)  # Write the previous line
                             x =1
 
                         # Write the current line that contains the college name
-                    if x == 1:
-                        ret += line + '\n'
+                    if x ==1:
+                        write_text_to_file(output_file_path, line)
 
                     # Update the previous line with the current line
                     previous_line = line  # Always update to the current line
@@ -64,24 +63,32 @@ def extract_text_from_pdf(pdf_path, college_names):
             else:
                 print(f"Page {page_num + 1}: No text found on this page.")
 
-    return ret
-
 #gets the output files ready to receive the information
 def clear_file(name_file):
     with open(name_file, 'w') as file:
         file.write("")
 
 #Skips the intro part of the pdf until it reaches the data itself
-def find_word_in_file(filestring, words_to_find):
-    words = filestring.split()
-    for word in words:
-        # Check if the word is in the list of words to find
-        if word in words_to_find:
-            return True  # Return the found word
-        else:
-            return False
-        
-    
+def find_word_in_file(filename, words_to_find):
+    try:
+        with open(filename, 'r') as file:
+            next(file)
+            for line in file:
+                # Split line into words
+                words = line.split()
+                x = 0
+                for word in words:
+                    # Check if the word is in the list of words to find
+                    if word in words_to_find:
+                        return True  # Return the found word
+                    else:
+                        return False
+        print("No words found from the list.")
+        return None  # Return None if no words were found
+    except FileNotFoundError:
+        print(f"The file {filename} does not exist.")
+        return None
+
 #adds the room information into the the underclass arrays   
 def add_to_class( Hall, Room, info_instance):
     colleges = {
@@ -170,56 +177,59 @@ def NCW_hall_name(words):
     elif Hall_name == "JOSE":
         return "JOSE FELICIANO", words[5]
 
-def store_intoArray(starts_College, filestring, info_instance):
-    lines = filestring.splitlines()
+def store_intoArray(starts_College, filename, info_instance):
     if starts_College:
-        for line in lines:
-            words = line.split()
-            words = [word.upper() for word in words]
-            first_word = words[0]
-            if first_word == "UPPERCLASS":
-                add_to_class(words[1], words[2], info_instance)
-            else:
-                if first_word == "NEW":
-                    Hall, Room = NCW_hall_name(words)
-                else:
-                    Hall, Room =words[2], words[3]
-                add_to_class(Hall, Room, info_instance)
-        
+        try:
+            with open(filename, 'r') as file:
+                next(file)
+                for line in file:
+                    words = line.split()
+                    words = [word.upper() for word in words]
+                    first_word = words[0]
+                    if first_word == "UPPERCLASS":
+                        add_to_class(words[1], words[2], info_instance)
+                    else:
+                        if first_word == "NEW":
+                            Hall, Room = NCW_hall_name(words)
+                        else:
+                            Hall, Room =words[2], words[3]
+                        add_to_class(Hall, Room, info_instance)
+        except FileNotFoundError:
+            print(f"The file {filename} does not exist.")
     else:
-        for line in lines:
-            words = line.split()
-            words = [word.upper() for word in words]
-            is_Upperclass = words[4]
-            is_Underclass = words[5]
-            if is_Upperclass.find("UPPERCLASS") != -1:
-                add_to_class(words[0], words[1], info_instance)
-            elif is_Underclass.find("COLLEGE") != -1:
-                add_to_class(words[0], words[1], info_instance)
-            else:
-                first_word = words[0]
-                if first_word == "ADDY":
-                    Hall, Room = "ADDY", words[2], 
-                elif first_word == "ALIYA":
-                    Hall, Room ="ALIYA KANJI", words[3]
-                elif first_word == "BOSQUE":
-                    Hall, Room ="FU", words[2]
-                elif first_word == "GROUSBECK":
-                    Hall, Room = "GROUSBECK", words[2]
-                elif first_word == "H":
-                    Hall, Room = "Hariri", words[2]
-                elif first_word == "JOSE":
-                    Hall, Room = "JOSE FELICIANO", words[4]
-                elif first_word == "KWANZA":
-                    Hall, Room = "KWANZA JONES", words[4]
-                elif first_word == "MANNION":
-                    Hall, Room = "MANNION", words[2]
-                try:
-                    add_to_class(Hall, Room, info_instance)
-                except Exception:
-                    return 
-    return
-
+        try:
+            with open(filename, 'r') as file:
+                next(file)
+                for line in file:
+                    words = line.split()
+                    words = [word.upper() for word in words]
+                    is_Upperclass = words[4]
+                    is_Underclass = words[5]
+                    if is_Upperclass.find("UPPERCLASS") != -1:
+                        add_to_class(words[0], words[1], info_instance)
+                    elif is_Underclass.find("COLLEGE") != -1:
+                        add_to_class(words[0], words[1], info_instance)
+                    else:
+                        first_word = words[0]
+                        if first_word == "ADDY":
+                            Hall, Room = "ADDY", words[2], 
+                        elif first_word == "ALIYA":
+                            Hall, Room ="ALIYA KANJI", words[3]
+                        elif first_word == "BOSQUE":
+                            Hall, Room ="FU", words[2]
+                        elif first_word == "GROUSBECK":
+                            Hall, Room = "GROUSBECK", words[2]
+                        elif first_word == "H":
+                            Hall, Room = "Hariri", words[2]
+                        elif first_word == "JOSE":
+                            Hall, Room = "JOSE FELICIANO", words[4]
+                        elif first_word == "KWANZA":
+                            Hall, Room = "KWANZA JONES", words[4]
+                        elif first_word == "MANNION":
+                            Hall, Room = "MANNION", words[2]
+                        add_to_class(Hall, Room, info_instance)
+        except FileNotFoundError:
+            print(f"The file {filename} does not exist.")
 
 def print_output(output, list):
     countUn = 0
@@ -236,7 +246,10 @@ def print_output(output, list):
 def main(file):
     # Paths to the PDF files
     pdf = Path(file)
-    pdf_output = Path("../temp.txt")
+    pdf_output = Path("/tmp/temp.txt")
+
+    with open(pdf_output, 'w') as file:
+        file.write("")
 
     uploaded_pdf = Info()
 
@@ -256,15 +269,13 @@ def main(file):
     }
 
     # Extract text from both PDFs
-    temp = extract_text_from_pdf(pdf, college_names)
+    extract_text_from_pdf(pdf, pdf_output, college_names)
     
 
     colleges = {"Butler", "Forbes", "Mathey", "New College West", "Rocky", "Upperclass", "Whitman", "Yeh"}
     
     print("uploaded test")
-    
-    
-    starts_College = find_word_in_file(temp, colleges)
-    store_intoArray(starts_College, temp, uploaded_pdf)
+    starts_College = find_word_in_file(pdf_output, colleges)
+    store_intoArray(starts_College, pdf_output, uploaded_pdf)
 
     return uploaded_pdf.Class
