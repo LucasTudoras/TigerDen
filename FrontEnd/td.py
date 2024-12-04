c6ea90657a4a8b7728478711491d908401a6b172
KaelMlinek <km1694@princeton.edu>
2024-12-04 14:03:20 -0500

    groups

--- FrontEnd/td.py
+++ FrontEnd/td.py
@@ -190,7 +190,7 @@ def leave_group():
                 """, (group_id[0],))
                 memeber_count = cursor.fetchone()[0]
                 cursor.execute(""" 
-                    SELECT admin_username FROM group WHERE group_id = %s
+                    SELECT admin_username FROM groups WHERE group_id = %s
                 """, (group_id[0],))
                 admin = cursor.fetchone()[0]
                 if memeber_count <=1 or admin== username:
